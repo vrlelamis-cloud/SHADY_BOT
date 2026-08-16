d5da10d792a56166e7c555eb43fac8f6613eb0da
vrlelamis-cloud <vrlelamis@gmail.com>
2026-08-16 15:41:03 +0100

    Update shady_bot.py

--- shady_bot.py
+++ shady_bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("8687815257:AAFg0Ets6JC4yS0gJNlsSKR7i9_xrHALE6I")
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
